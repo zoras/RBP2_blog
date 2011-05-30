32e03d034b8e40070d58f50a726162e99885d601
zoras <sawrows@gmail.com>
2011-05-30 09:25:52 +0545

    unchanged comments contrlloer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,30 @@
 class CommentsController < ApplicationController
   before_filter :login_required
-  before_filter :find_post
-  
+
   def new
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.build
+
+    respond_to do |format|
+      format.html
+      format.xml
+    end
   end
-  
+
   def create
-    @comment = @post.comments.build(params[:comment])
-    if @comment.save
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(params[:comment])
+    @comment.post_id = @post.id
+    @comment.user_id = current_user.id
+
+    if @comment.is_minimum_length? && @comment.save
       flash[:notice] = "Comment has been created."
-      redirect_to @post
+      redirect_to user_post_path(current_user,@post)
     else
       flash[:notice] = "Comment has not been created."
       render :action => "new"
     end
   end
-  
-  private
-    def find_post
-      @post = Post.find(params[:post_id])
-    end
-  
+
 end
+
