697dcc0b05079e1eede8e01d37aa28d247396b60
zoras <sawrows@gmail.com>
2011-05-30 09:08:58 +0545

    refactored application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
-  
+
+  helper_method :current_user, :logged_in?
+
   private
-  
+
   def current_user
     current_user_session && current_user_session.record
   end
-  
+
   def current_user_session
     UserSession.find
   end
-  
+
   def login_required
     redirect_to root_path, :notice => "You must be logged in to do that." if !logged_in?
   end
-  
+
   def logged_in?
-    !!current_user
+    current_user
   end
-      
-  
-  helper_method :current_user, :logged_in?
+
+
 end
+
